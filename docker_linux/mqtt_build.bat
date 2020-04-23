3f90c8ef96f18a68f532779080a45a382d7a67f4
nmcdonnell-kx <nmcdonnell@kx.com>
2020-04-23 10:28:15 +0100

    Bit less of an error-prone way to bring over scripts to linux docker from a windows git clone (in case git config has autocrlf=true).  There is an open issue against docker-win to support line ending conversion when copying from Windows into the container but requires this workaround for now.

--- docker_linux/mqtt_build.bat
+++ docker_linux/mqtt_build.bat
@@ -5,6 +5,6 @@ SET PAHO_MQTT="c:\git\paho.mqtt.c"
 SET QHOME_LINUX="c:\linux_shared\q"
 
 docker build -f Dockerfile.build -t mqtt-dev .
-docker run --rm -it -v %MQTT_SOURCE%:/source/mqtt -v %PAHO_MQTT%:/source/paho.mqtt.c -v %QHOME_LINUX%:/q mqtt-dev /bin/bash -c /source/mqtt/docker_linux/mqtt_build.sh
+docker run --rm -it -v %MQTT_SOURCE%:/source/mqtt -v %PAHO_MQTT%:/source/paho.mqtt.c -v %QHOME_LINUX%:/q mqtt-dev /bin/bash -c /source/mqtt_build.sh
 
 ENDLOCAL
