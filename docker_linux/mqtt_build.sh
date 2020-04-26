73dacedfb4d878428624a67b882f116f95cbab2b
nmcdonnell-kx <nmcdonnell@kx.com>
2020-04-26 20:37:25 +0100

    make install of lib.so rather than symlink on linux Typo in travis file

--- docker_linux/mqtt_build.sh
+++ docker_linux/mqtt_build.sh
@@ -3,8 +3,7 @@
 make --directory /source/paho.mqtt.c
 make install --directory /source/paho.mqtt.c
 make --directory /source/mqtt
-
-ln -s -f /source/mqtt/mqtt.so /source/mqtt/examples/mqtt.so
+make install --directory /source/mqtt
 
 /bin/bash
 
