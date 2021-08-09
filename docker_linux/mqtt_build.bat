6842fa69d03ea6a3f75d198a96a69540d68356f9
Paul Grainger <pagrainger@gmail.com>
2021-08-09 09:20:51 -0700

    Adding dockerfile to build on alpine

--- docker_linux/mqtt_build.bat
+++ docker_linux/mqtt_build.bat
@@ -3,7 +3,7 @@ SETLOCAL
 SET MQTT_SOURCE="C:\Users\guest\Development\mqtt"
 SET QHOME_LINUX="C:\q"
 
-docker build -f Dockerfile.build -t mqtt-dev .
+docker build -f Dockerfile.centos7 -t mqtt-dev .
 docker run --rm -it -v %MQTT_SOURCE%:/source/mqtt -v %QHOME_LINUX%:/q mqtt-dev /bin/bash -c /source/mqtt_build.sh
 
 ENDLOCAL
