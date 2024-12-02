bc533273c872a67b7ffb6acdd453a867e9d6848b
Simon Shanks <sshanks@kx.com>
2024-12-02 20:24:41 +0000

    paho lib version changed from 1.3.12 to 1.3.13

--- travis_setup.sh
+++ travis_setup.sh
@@ -3,16 +3,14 @@
 mkdir cbuild
 
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-  wget https://github.com/eclipse/paho.mqtt.c/releases/download/v1.3.12/Eclipse-Paho-MQTT-C-1.3.12-Darwin.tar.gz.zip
-  unzip Eclipse-Paho-MQTT-C-1.3.12-Darwin.tar.gz.zip
-  tar xvf Eclipse-Paho-MQTT-C-1.3.12-Darwin.tar.gz -C ./cbuild --strip-components=1
+  wget https://github.com/eclipse/paho.mqtt.c/releases/download/v1.3.13/Eclipse-Paho-MQTT-C-1.3.13-Darwin.tar.gz
+  tar xvf Eclipse-Paho-MQTT-C-1.3.13-Darwin.tar.gz -C ./cbuild --strip-components=1
 elif [ "$TRAVIS_OS_NAME" == "linux" ]; then
-  wget https://github.com/eclipse/paho.mqtt.c/releases/download/v1.3.12/Eclipse-Paho-MQTT-C-1.3.12-Linux.tar.gz.zip
-  unzip Eclipse-Paho-MQTT-C-1.3.12-Linux.tar.gz.zip
-  tar xvf Eclipse-Paho-MQTT-C-1.3.12-Linux.tar.gz -C ./cbuild --strip-components=1
+  wget https://github.com/eclipse/paho.mqtt.c/releases/download/v1.3.13/Eclipse-Paho-MQTT-C-1.3.13-Linux.tar.gz
+  tar xvf Eclipse-Paho-MQTT-C-1.3.13-Linux.tar.gz -C ./cbuild --strip-components=1
 elif [ "$TRAVIS_OS_NAME" == "windows" ]; then
-  wget https://github.com/eclipse/paho.mqtt.c/releases/download/v1.3.12/eclipse-paho-mqtt-c-win64-1.3.12.zip
-   7z x -ocbuild eclipse-paho-mqtt-c-win64-1.3.12.zip
+  wget https://github.com/eclipse/paho.mqtt.c/releases/download/v1.3.13/eclipse-paho-mqtt-c-win64-1.3.13.zip
+   7z x -ocbuild eclipse-paho-mqtt-c-win64-1.3.13.zip
 else
   echo "$TRAVIS_OS_NAME is currently not supported"  
 fi
